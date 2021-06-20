618b90e202542ad0c8853aa9dca46da3efa2cd0e
Tony <trong.dm.it@gmail.com>
2021-06-20 20:24:41 +0700

    Upcode

---
+++ poc_mvn_gcp/src/main/java/org/vcm/demo_load_file_service/App.java
@@ -0,0 +1,14 @@
+package org.vcm.demo_load_file_service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+//        sleep(400);
+    }
+}
